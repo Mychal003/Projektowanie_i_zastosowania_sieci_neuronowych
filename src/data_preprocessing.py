075e6f7ef1a6a981be8f22a40a5e5d64836b718d
Michał Pawlik <pawlikm11@gmail.com>
2025-04-05 15:09:11 +0200

    preprocessing, zaczeta eda

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,25 @@
-'''Skrypty do wstępnego przetwarzania danych'''
+import pandas as pd
+import os
+
+def preprocess():
+    input_path = "C:\\Users\\pawli\\Desktop\\sieci\\Projektowanie_i_zastosowania_sieci_neuronowych\\data\\raw\\raw.csv"
+    output_dir = "C:\\Users\\pawli\\Desktop\\sieci\\Projektowanie_i_zastosowania_sieci_neuronowych\\data\\processed"
+    output_path = os.path.join(output_dir, "cleaned_data.csv")
+
+    # Wczytanie danych
+    df = pd.read_csv(input_path)
+
+    # Konwersja "Date" do datetime
+    df["Date"] = pd.to_datetime(df["Date"])
+    df = df.sort_values(by="Date").reset_index(drop=True)
+
+    # Usuwanie znaku dolara i konwersja do float
+    for i in ["Close/Last", "Open", "High", "Low"]:
+        df[i] = df[i].str.replace("$", "", regex=False)
+        df[i] = pd.to_numeric(df[i])
+
+    # Zapis przetworzonych danych
+    os.makedirs(output_dir, exist_ok=True)
+    df.to_csv(output_path, index=False)
+
+    return df
--- src/eda.py
+++ src/eda.py
@@ -1,1 +1,24 @@
-'''Funkcje pomocnicze do EDA'''
+# src/eda.py
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_summary(df):
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(data=df, x='Date', y='Close/Last')
+
+    step = max(1, len(df) // 7)
+    x_ticks = df['Date'].iloc[::step]
+    x_labels = df['Date'].dt.strftime('%B').iloc[::step]
+
+    y_min, y_max = df['Close/Last'].min(), df['Close/Last'].max()
+    y_ticks = [round(y_min + i * (y_max - y_min) / 5, 2) for i in range(6)]
+
+    plt.xticks(ticks=x_ticks, labels=x_labels, rotation=30)
+    plt.yticks(y_ticks)
+
+    plt.xlabel('Miesiąc roku 2024')
+    plt.ylabel('Cena zamknięcia [USD]')
+    plt.title('Ceny akcji spółki')
+    plt.tight_layout()
+    plt.show()
